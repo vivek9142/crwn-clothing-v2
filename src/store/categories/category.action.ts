3e1cdc721d0f17e6c79996e8b8d5383842e225e9
Vivek <vivek9142@gmail.com>
2022-06-10 19:37:09 +0530

    commmented documentation for reducer  and action TS impl and added reducer state acc to TS

--- src/store/categories/category.action.ts
+++ src/store/categories/category.action.ts
@@ -1,7 +1,32 @@
+/*
+we finished updating our width matcher function that takes some action creator function
+and adds an additional functionality of them so that they can determine if a past action
+has the same type as the corresponding action that they create.
 
+let's import it in and then what we're going to do is use with matcher
+and wrap around each and every one of these different action creators.
+So these action creators we've made so far are going to be the argument that gets 
+passed to with matcher. And now what happens is that each of these are Matchable 
+action creators.They now have that extended dot match method.
+
+So here this is an action of the type categories.
+Fetch, action, start.
+So we're reaching into this function, getting this action type, and then getting 
+the actual type from the enum and setting that to some type value at the top level 
+of the actual function itself.Similarly, if you check the match function, 
+it is also a faction that receives some action of any action.
+And it types that predicate to fetch category start if it passes the match.
+
+So that's really all we're doing here.
+Now we're going to use these actions inside of the reducer.
+So now that's category start.Fetch categories.Success.Fetch.Categories.Failed.
+
+So remember, the original problem we're aiming to solve is that we want to make 
+it so that this reducer is way more accurate to what's going to actually happen.
+*/
 import { CATEGORIES_ACTION_TYPES , Category} from './category.types';
 
-import { createAction , Action , ActionWithPayload } from '../../utils/reducer/reducer.utils';
+import { createAction , Action , ActionWithPayload , withMatcher} from '../../utils/reducer/reducer.utils';
 
 
 export type FetchCategoriesStart = Action<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START>
@@ -13,14 +38,17 @@ export type FetchCategoriesFailed = ActionWithPayload<CATEGORIES_ACTION_TYPES.FE
 export type CategoryAction = FetchCategoriesStart | FetchCategoriesSuccess | FetchCategoriesFailed;
 
 
-export const fetchCategoriesStart = ():FetchCategoriesStart =>
-  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+export const fetchCategoriesStart = withMatcher(
+  ():FetchCategoriesStart =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START));
 
-export const fetchCategoriesSuccess = (categoriesArray:Category[]):FetchCategoriesSuccess =>
-  createAction(
-    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
-    categoriesArray
-  );
+export const fetchCategoriesSuccess = withMatcher(
+  (categoriesArray:Category[]):FetchCategoriesSuccess =>
+    createAction(
+      CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+      categoriesArray
+    ));
 
-export const fetchCategoriesFailed = (error:Error):FetchCategoriesFailed =>
-  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+export const fetchCategoriesFailed = withMatcher(
+  (error:Error):FetchCategoriesFailed =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error));
--- src/store/categories/category.reducer.ts
+++ src/store/categories/category.reducer.ts
@@ -1,6 +1,35 @@
-import { CATEGORIES_ACTION_TYPES ,Category} from './category.types';
+/*
+So remember, the original problem we're aiming to solve is that we want to make it so that 
+this reducer is way more accurate to what's going to actually happen.
+Essentially, each of these actions that come in can be any action at all.
+So let's import in any action from Redux.
 
-import { CategoryAction } from './category.action';
+So instead of selling this to this discriminating union of category action, 
+we know it can be any action.So now what we're going to do instead of using 
+this switch statement is we're going to say, okay,
+if the action that you give me matches fetch category start.So I'm say if that's 
+category start match the action.
+If it matches, if you hover over this action now, what do you see?
+You see that the action has been narrowed.It must be of fetch category start.
+That's what we're doing here.
+If it matches, it must be of this type.
+So now what we're going to do is return back to this category state except 
+is loading is true.
+
+Now what you'll see, though, is that we still have an error inside of category state.
+The reason why when you hover over it, it says function lacks ending return statement
+and return type does not include undefined.
+
+So here we've explicitly stated that this function always 
+returns you back some category state, but clearly that is not what's happening.So in order 
+for this to work, we must explicitly return.If none of these actions match any of our 
+corresponding action creators, then what we do is we return
+the default state.And now our categories reducer is fully type safe.
+*/
+import {Category} from './category.types';
+
+import {fetchCategoriesStart , fetchCategoriesSuccess , fetchCategoriesFailed} from './category.action';
+import { AnyAction } from 'redux';
 
 export type CategoriesState = {
   readonly categories: Category[];
@@ -16,17 +45,32 @@ export const CATEGORIES_INITIAL_STATE:CategoriesState = {
 
 export const categoriesReducer = (
   state = CATEGORIES_INITIAL_STATE,
-  action = {} as CategoryAction
-) => {
+  // action = {} as CategoryAction
+  action = {} as AnyAction
+):CategoriesState => {
   
-  switch (action.type) {
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
-      return { ...state, isLoading: true };
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-      return { ...state, categories: action.payload, isLoading: false };
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-      return { ...state, error: action.payload, isLoading: false };
-    default:
-      return state;
+  if(fetchCategoriesStart.match(action)){
+    return { ...state, isLoading: true };
   }
+  
+  if(fetchCategoriesSuccess.match(action)){
+    return { ...state, categories: action.payload, isLoading: false };
+  }
+
+  if(fetchCategoriesFailed.match(action)){
+    return { ...state, error: action.payload, isLoading: false };
+  }
+
+  return state;
+
+  // switch (action.type) {
+  //   case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
+  //     return { ...state, isLoading: true };
+  //   case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
+  //     return { ...state, categories: action.payload, isLoading: false };
+  //   case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
+  //     return { ...state, error: action.payload, isLoading: false };
+  //   default:
+  //     return state;
+  // }
 };
