e608d92578a485a54195a7fe5603f15ab9e03474
Vivek <vivek9142@gmail.com>
2022-06-12 03:35:07 +0530

    updated Cart reducer and cateory reducer TS impl

--- src/store/cart/cart.action.ts
+++ src/store/cart/cart.action.ts
@@ -1,7 +1,7 @@
 import { CategoryItem } from '../categories/category.types';
 
 import { CART_ACTION_TYPES , CartItem } from './cart.types';
-import { Action , ActionWithPayload, createAction ,withMatcher } from '../../utils/reducer/reducer.utils';
+import {ActionWithPayload, createAction ,withMatcher } from '../../utils/reducer/reducer.utils';
 
 const addCartItem = (cartItems:CartItem[], productToAdd:CategoryItem):CartItem[] => {
   const existingCartItem = cartItems.find(
@@ -26,15 +26,6 @@ const removeCartItem = (cartItems:CartItem[], cartItemToRemove:CartItem): CartIt
     (cartItem) => cartItem.id === cartItemToRemove.id
   );
 
-  // check if quantity is equal to 1, if it is remove that item from the cart
-
-  /*
-  in this particular case, when you check on this quantity value inside of JavaScript, 
-  JavaScript will give you back undefined if there is no existing card item.
-  But for us this is not good.We need to actually modify this code so that it works.
-  And all we can do is just say if there is an existing card item, then check the quantity.
-  Otherwise, don't run this part
-  */
 
   if ( existingCartItem && existingCartItem.quantity === 1) {
     return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
@@ -51,26 +42,11 @@ const removeCartItem = (cartItems:CartItem[], cartItemToRemove:CartItem): CartIt
 const clearCartItem = (cartItems: CartItem[], cartItemToClear: CartItem): CartItem[] =>
   cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
 
-/*
-We have now typed out all of our utility functions.
-What we want to do next is we want to define each of our actions as a type.*/
 
 export type SetIsCartOpen = ActionWithPayload<CART_ACTION_TYPES.SET_IS_CART_OPEN,boolean>;
 
-/*
-we need to think about these three actions.
-Add item to cart,Remove item from cart and clear item from cart.
-Each of these, while being a different action creator, actually does in the end 
-create the exact same set cart items action.
-So technically, these three are different functions that manufacture the same type 
-of action.And here we're just defining the type of action.
-And this type of action is our set cart items action.
-*/
 export type SetCartItems =  ActionWithPayload<CART_ACTION_TYPES.SET_CART_ITEMS, CartItem[]>;
 
-//As for these other three, we need to first create this set cart items.Action Creator.
-//As for sending action with createaction instead, 
-//we can simply replace this with set cart items.Passing in new cart items
 
 export const setCartItems = withMatcher(
   (cartItems: CartItem[]): SetCartItems =>
--- src/store/cart/cart.types.ts
+++ src/store/cart/cart.types.ts
@@ -1,15 +1,3 @@
-/*
-we are storing car items in an array.Cart items is a specific set of data in an array.
-But what is that data? if we remember inside of cart actions, our cart item is the
-first time where we actually create this actual cart item.
-And what it is, is all of the properties of the product that we're adding in, 
-plus a quantity value,which is a number.
-
-Now, if you think about the category item, it has all these properties we want, 
-but it just missing the quantity.So here we can make an intersection type of item 
-which is equal to category item, plus this additional
-quantity field that we're looking for, which is a number.
-*/
 
 import { CategoryItem } from "../categories/category.types";
 
